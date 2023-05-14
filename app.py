4413474f60e9e68e1107c1ae676c6335db6c0375
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-05-14 15:25:47 -0300

    nova atualização app.py

--- app.py
+++ app.py
@@ -38,9 +38,24 @@ def index_view():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-     x_axis = request.form.get('dado')
-     return jsonify({'placement':x_axis})
-
+     @app.route('/predict',methods=['POST'])
+    def predict():
+         file = request.files['file']
+         classes_x = read_text_file(file)
+         class_prediction = np.argmax(loaded_model.predict(classes_x),axis=1)
+     result = class_prediction
+     if result.max() == 0:
+        return jsonify({'placement':('Andando')})
+     if result.max() == 1:
+        return jsonify({'placement':'Correndo'})
+     if result.max() == 2:
+        return jsonify({'placement':'Subindo Escadas'})
+     if result.max() == 3:
+        return jsonify({'placement':'Descendo Escadas'})
+     if result.max() == 4:
+        return jsonify({'placement':'Em Pé'})
+     if result.max() == 5:
+        return jsonify({'placement':'Deitado'})
 
 
 
