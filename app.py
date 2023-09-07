b00086a84dfe6b79f095dec9f6e9a57b415c0487
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-09-07 12:55:48 -0300

    Nova atualização do app.py

--- app.py
+++ app.py
@@ -15,34 +15,37 @@ from tensorflow.keras.models import load_model
 app = Flask(__name__)
 
 
-#%%
+# Carregue o modelo uma vez ao iniciar o servidor Flask
+model = load_model('./model.h5')
+
 @app.route('/api', methods=['POST'])
 def predict():
-   # try:
+    try:
         # Obtém o JSON da solicitação
         data = request.get_json()
-        #Loading CNN model
-        load_model('./model.h5')
-            #load_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-            #print('Modelo carregdo e compilado com sucesso!!!')
-            #pré-processamento
-        columns = ['x','y','z']
-        df = pd.DataFrame(data,columns=columns)
+
+        # Pré-processamento dos dados
+        columns = ['x', 'y', 'z']
+        df = pd.DataFrame(data, columns=columns)
         df['x'] = df['x'].astype('float')
         df['y'] = df['y'].astype('float')
         df['z'] = df['z'].astype('float')
         data = df.to_numpy()
-        #print(df.isnull())
-        #data = df.to_numpy()
-        data = data.reshape(-1,80,3)
-        model = load_model('model.h5')
-        for _ in range(1):
-            class_predict = np.argmax(model.predict(data),axis=1)
-        mapeamento = {0:'Downstairs', 1:'Jogging', 2:'Sitting', 3:'Standing',4:'Upstairs',5:'Walking'}
-        rotulos_mapeados = [mapeamento[v] for v in class_predict]
-    
+        data = data.reshape(-1, 80, 3)
+
+        # Faça uma única previsão com o modelo carregado
+        class_predict = np.argmax(model.predict(data), axis=1)
+
+        mapeamento = {0: 'Downstairs', 1: 'Jogging', 2: 'Sitting', 3: 'Standing', 4: 'Upstairs', 5: 'Walking'}
+        rotulos = [mapeamento[v] for v in class_predict]
+
+        return jsonify({'args': str(rotulos)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
+
         return jsonify({'args':str(rotulos_mapeados)})
 
 app.run(debug=False,host='0.0.0.0',port=5000)
-
-
