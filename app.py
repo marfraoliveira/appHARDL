ff3728d0f59e48da6b557d995ae1095d150eeb5c
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-09-06 22:22:55 -0300

    nova_API.py

--- app.py
+++ app.py
@@ -7,62 +7,41 @@ import pandas as pd
 import keras.models
 from keras.models import model_from_json
 import json
-from json import JSONEncoder
+#from json import JSONEncoder
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
-def api_endpoint():
-    # Obtém os dados JSON da solicitação
-    data = request.json  
-    #Loading CNN model
-    json_file = open('model.json','r')
-    load=json_file.read()
-    json_file.close()
-    load_model =model_from_json(load)
-    load_model.load_weights('./model.h5')
-    load_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    print('Modelo carregdo e compilado com sucesso!!!')
-    #pré-processamento
-    columns = ['x','y','z']
-    df = pd.DataFrame(request.get_json())
-    df = pd.DataFrame(df, columns = columns)
-    df['x'] = df['x'].astype('float')
-    df['y'] = df['y'].astype('float')
-    df['z'] = df['z'].astype('float')
-      
-    #print(df.isnull())
-    data = df.to_numpy()
-    data[0].ndim
-   
-    data = data.reshape(-1,80,3)
-    class_predict = np.argmax(load_model.predict(data),axis=0)
-    
-     
-    if class_predict.max() == 0:
-        return jsonify({'placement':str(class_predict)})
-    if class_predict.max() == 1:
-        return jsonify({'placement':str(class_predict)})
-    if class_predict.max() == 2:
-       return jsonify({'placement':str(class_predict)})
-    if class_predict.max() == 3:
-       return jsonify({'placement':str(class_predict)})
-    if class_predict.max() == 4:
-       return jsonify({'placement':str(class_predict)})
-        
-    else:
-       return jsonify({'placement':str(class_predict)})
-    
-    
-    # Realiza algum processamento com os dados e prepara uma resposta JSON
-    response_data = {
-        'message': 'Dados recebidos com sucesso',
-        'received_data': data
-    }
-    
-    return jsonify(response_data)
-    
 
-app.run(debug=False,host='0.0.0.0',port=3000)
+#%%
+@app.route('/api', methods=['POST'])
+def predict():
+   # try:
+        # Obtém o JSON da solicitação
+        data = request.get_json()
+        #Loading CNN model
+        load_model('./model.h5')
+            #load_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+            #print('Modelo carregdo e compilado com sucesso!!!')
+            #pré-processamento
+        columns = ['x','y','z']
+        df = pd.DataFrame(data,columns=columns)
+        df['x'] = df['x'].astype('float')
+        df['y'] = df['y'].astype('float')
+        df['z'] = df['z'].astype('float')
+        data = df.to_numpy()
+        #print(df.isnull())
+        #data = df.to_numpy()
+        data = data.reshape(-1,80,3)
+        model = load_model('./model.h5')
+        for _ in range(1):
+            class_predict = np.argmax(model.predict(data),axis=1)
+        mapeamento = {0:'Downstairs', 1:'Jogging', 2:'Sitting', 3:'Standing',4:'Upstairs',5:'Walking'}
+        rotulos_mapeados = [mapeamento[v] for v in class_predict]
+    
+        return jsonify({'args':str(rotulos_mapeados)})
+
+app.run(debug=False,host='0.0.0.0',port=5000)
 
